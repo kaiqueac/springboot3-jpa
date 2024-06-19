9ca14344dab6712d4a5389e880e579e970ddfa58
Kaique Costa <kaiqueaccontato@gmail.com>
2024-06-19 13:50:28 -0300

    Anotações

--- src/main/java/com/meuportifolio/course/config/TestConfig.java
+++ src/main/java/com/meuportifolio/course/config/TestConfig.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
 
-    @Autowired
+    @Autowired // independence injection
     private UserRepository userRepository;
 
 
@@ -23,6 +23,7 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
+        // add users in list
         userRepository.saveAll(Arrays.asList(u1, u2));
 
     }
