fab73a649509846b0454de7d4f1d300b3b09a6af
Kaique Costa <kaiqueaccontato@gmail.com>
2024-06-19 13:30:06 -0300

    Banco de dados H2, test profile, JPA

--- src/main/java/com/meuportifolio/course/resources/UserResource.java
+++ src/main/java/com/meuportifolio/course/resources/UserResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/users") // show users in localhost:8080/users
 public class UserResource {
 
     @GetMapping
