aca458a105cc98ee078a10efcf44b8bf61b1a05a
Kaique Costa <kaiqueaccontato@gmail.com>
2024-06-19 19:32:03 -0300

    Camada de serviço, registro de componentes

--- src/main/java/com/meuportifolio/course/repositories/UserRepository.java
+++ src/main/java/com/meuportifolio/course/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.meuportifolio.course.repositories;
 
 import com.meuportifolio.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository // optional annotation
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
