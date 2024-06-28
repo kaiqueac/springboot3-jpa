8f16bd40cba0ea9f35b357d5aa7cff20c44bda1b
Kaique Costa <kaiqueaccontato@gmail.com>
2024-06-28 19:39:13 -0300

    Entidade Category

---
+++ src/main/java/com/meuportifolio/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.meuportifolio.course.repositories;
+
+import com.meuportifolio.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+}
